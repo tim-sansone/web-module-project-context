e68892b1f97b66a290ed2c1f3c4fafa9e42c8bcd
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-06-17 16:34:47 -0500

    add item function works

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
+	console.log(cart);
+
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
