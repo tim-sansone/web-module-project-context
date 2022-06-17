383e3481797b449dba7a719565713de4afe7a0e9
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-06-17 17:30:00 -0500

    remove item complete

--- src/App.js
+++ src/App.js
@@ -19,10 +19,14 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }} >
-				<CartContext.Provider value={{cart}}>
+				<CartContext.Provider value={{cart, removeItem}}>
 					<Navigation />
 
 					<Route exact path="/">
